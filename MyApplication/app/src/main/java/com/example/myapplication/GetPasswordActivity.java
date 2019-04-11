f1e49cd9eb0740501a7a7ac62c7041af5bb52431
Yasmeen Sayed <34556045+yasoo-sayed@users.noreply.github.com>
2019-04-11 04:29:33 +0200

    update5

--- MyApplication/app/src/main/java/com/example/myapplication/GetPasswordActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GetPasswordActivity.java
@@ -7,15 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class GetPasswordActivity extends AppCompatActivity {
     private String email,password;
@@ -30,6 +35,9 @@ public class GetPasswordActivity extends AppCompatActivity {
         email = intent.getStringExtra("email");
         System.out.println("email in pass: "+email);
 
+        sendToDatabase();
+        jasonParse();
+
         ok=(Button)findViewById(R.id.btn);
 
         ok.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +50,37 @@ public class GetPasswordActivity extends AppCompatActivity {
         });
     }
 
+    public void sendToDatabase() {
+        RequestQueue queue = Volley.newRequestQueue(GetPasswordActivity.this);
+        String url = "http://192.168.1.3:8383/getEmail";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        if (response.contains("true")) {
+                            finish();
+                            Intent intent = new Intent(GetPasswordActivity.this, RequestsActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.err.println("error: " + error.getMessage());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+    }
+
     public void jasonParse() {
         String url = "http://192.168.1.3:8383/sendPassword";
         RequestQueue queue = Volley.newRequestQueue(GetPasswordActivity.this);
@@ -49,13 +88,7 @@ public class GetPasswordActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        try {
-                            JSONObject jsonObject = response.getJSONObject("password");
-                            password = jsonObject.getString("password");
-                            ((EditText) findViewById(R.id.txt2)).setText(password);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        ((EditText) findViewById(R.id.txt2)).setText(response.toString());
                     }
                 }, new Response.ErrorListener() {
             @Override
